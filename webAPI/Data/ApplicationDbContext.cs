c9c0939e8ffcee92bde89a472fa130444f76e8ea
Иван Бажанов <bazhanovivan@yandex.ru>
2019-09-01 19:23:53 +0400

    Добавьте файлы проекта.

---
+++ webAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using JetBrains.Annotations;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webAPI.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ webAPI/Data/SeedDatabase.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webAPI.Data
+{
+    public class SeedDatabase
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            var context = serviceProvider.GetRequiredService<ApplicationDbContext>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            context.Database.EnsureCreated();
+
+            if (!context.Users.Any())
+            {
+                ApplicationUser user = new ApplicationUser()
+                {
+                    Email = "ivan@ivan.com",
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                    UserName = "Ivan"
+                };
+                userManager.CreateAsync(user, "Password@123");
+            }
+
+        }
+    }
+}
